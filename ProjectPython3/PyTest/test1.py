64aa0f97bb535e7d4de5210390946ee5e666f8e3
berag <berag@AIBWTA5RYL.asiapacific.hpqcorp.net>
2018-05-21 16:42:30 +0530

    DataScience

--- ProjectPython3/PyTest/test1.py
+++ ProjectPython3/PyTest/test1.py
@@ -7,10 +7,54 @@ Created on May 21, 2018
 if __name__ == '__main__':
     pass
 
+import numpy as np
+import pandas as pd
+#import matplotlib as plt
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
 
-print('Hello Python for Git')
-print('---------------------------------')
 
-print('Next Change')
+b = np.array([1,5,8,9,4,7,5])
+print(b)
+
+df = pd.DataFrame(data = b)
+print(df)
+
+# matplotlib
+t = np.array([1,5,8,9,4,7,5])
+s = np.array([1,2,3,4,5,6,7])
+plt.plot(t, s)
+
+plt.xlabel('x - t array')
+plt.ylabel('y - s array')
+plt.title('Simple Plot')
+plt.grid(True)
+#plt.savefig("test.png")
+plt.show()
+
+# seaborn
+sns.set_color_codes("dark")
+_ = plt.plot([0, 1], color="g")
+_ = plt.plot([0, 2], color="m")
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
