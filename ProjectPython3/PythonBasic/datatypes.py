e89c1dea503fe75babd043ce69699b3aa7391d9f
GourBera <g.bera@gmail.com>
2023-02-20 09:33:23 +0530

    Feb-20

--- ProjectPython3/PythonBasic/datatypes.py
+++ ProjectPython3/PythonBasic/datatypes.py
@@ -1,11 +1,20 @@
-list = [1,2,3,4,5,6,7,8,9]
-dict = {'key1': 10, 'key2':20, 'key3':30}
-tuple = (10,20,30,40,50,60)
-set = {10,20,30,40,50}
+Collections
+    namedtuple( )
+    deque
+    ChainMap
+    Counter
+    OrderedDict
+    defaultdict
 
-print(list)
-print(dict)
-print(tuple)
-print(set)
+Itertools
+    count(start=0, step=1): Returns an infinite iterator that generates an arithmetic progression starting from start and incrementing by step for each iteration.
+    cycle(iterable): Returns an infinite iterator that repeats the values in iterable indefinitely.
+    repeat(elem, n=None): Returns an iterator that repeats the value elem n times, or indefinitely if n is not provided.
+    chain(*iterables): Returns a single iterator that combines multiple iterables into a single sequence.
+    groupby(iterable, key=None): Returns an iterator that groups the elements of iterable based on the value returned by key.
+    islice(iterable, start, stop, step=1): Returns an iterator that returns a slice of the elements in iterable starting from start and up to but not including stop, with a step size of step.
+    starmap(function, iterable): Returns an iterator that applies function to each element of iterable as a tuple.
+    tee(iterable, n=2): Returns n independent iterators that each return the elements of iterable.
+    zip_longest(*iterables, fillvalue=None): Returns an iterator that aggregates the elements of iterables into tuples, filling in missing values with fillvalue.
 
 
